3365c78b4ac184a40ea3dd5921891d46fcf8ea7b
cmidgley <cmidgley@10.45.1.103>
2014-08-12 23:06:30 -0400

    Fixed copyright header typo

--- src/commands/CommandShowSign.java
+++ src/commands/CommandShowSign.java
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with theunibot.  If not, see <http://www.gnu.org/licenses/>.
 
     Copyright (c) 2014 Unidesk Corporation
  */
